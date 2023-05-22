cf92cf3f7fa1bd7d906008a7f28e20993b08a334
Sharid Madrid <sharid.madrid@udea.edu.co>
2023-05-22 17:23:55 -0500

    formulario para crear categorias

--- src/pages/Admin/CategoriesAdmin.js
+++ src/pages/Admin/CategoriesAdmin.js
@@ -7,20 +7,51 @@ import { ModalBasic } from "../../components/Common";
 import { useCategory } from "../../hooks";
 
 export function CategoriesAdmin() {
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState(null);
+  const [contentModal, setContentModal] = useState(null);
+  const [refetch, setRefetch] = useState(false);
+  const { loading, categories, getCategories, deleteCategory } = useCategory();
+
+  useEffect(() => getCategories(), [refetch]);
+
+  const openCloseModal = () => setShowModal((prev) => !prev);
+  const onRefetch = () => setRefetch((prev) => !prev);
+
+  const addCategory = () => {
+    setTitleModal("Nueva categoria");
+    setContentModal(
+      <AddEditCategoryForm onClose={openCloseModal} onRefetch={onRefetch} />
+    );
+    openCloseModal();
+  };
+
+
   return (
     <>
-      <HeaderPage
-        title="Categorias"
-        btnTitle="Nueva categoria"
-        btnClick={addCategory}
+    <HeaderPage
+      title="Categorias"
+      btnTitle="Nueva categoria"
+      btnClick={addCategory}
+    />
+    {loading ? (
+      <Loader active inline="centered">
+        Cargando...
+      </Loader>
+    ) : (
+      <TableCategoryAdmin
+        categories={categories}
+        updateCategory={updateCategory}
+        deleteCategory={onDeleteCategory}
       />
-      {loading ? (
-        <Loader active inline="centered">
-          Cargando...
-        </Loader>
-      ) : (
-        <h2>Lista de Categorias</h2>
-      )}
-      </>
+    )}
+
+    <ModalBasic
+      show={showModal}
+      onClose={openCloseModal}
+      title={titleModal}
+      children={contentModal}
+    />
+    </>
   );
 }
