f4b2101f6eca20736be68b5763337df650d6d4e7
Sharid Madrid <sharid.madrid@udea.edu.co>
2023-05-22 23:10:48 -0500

    organizamos para que nos muestre la info real

--- src/api/payment.js
+++ src/api/payment.js
@@ -37,36 +37,3 @@ export async function getPaymentByTableApi(idTable) {
     throw error;
   }
 }
-
-export async function closePaymentApi(idPayment) {
-  try {
-    const url = `${BASE_API}/api/payments/${idPayment}/`;
-    const params = {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        statusPayment: PAYMENT_STATUS.PAID,
-      }),
-    };
-    await fetch(url, params);
-  } catch (error) {
-    throw error;
-  }
-}
-
-export async function getPaymentsApi() {
-  try {
-    const paymentFilter = `statusPayment=${PAYMENT_STATUS.PAID}`;
-    const orderingFilter = "ordering=created_at";
-
-    const url = `${BASE_API}/api/payments/?${paymentFilter}&${orderingFilter}`;
-
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    throw error;
-  }
-}
